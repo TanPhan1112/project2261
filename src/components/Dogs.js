fde400306a7e1e8d1ecaa4962f9e809da5589d2d
TanPhan1112 <minhtan111293@gmail.com>
2021-07-29 14:29:12 -0700

    project updated

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -6,8 +6,6 @@ import Footer from "./Footer";
 
 function Dogs() {
     const [dogs, setDogs] = useState([]);
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
     const [q, setQ] = useState("");
 
     useEffect(() => {
@@ -15,12 +13,10 @@ function Dogs() {
             .then(result => {
                 const dogs = Object.keys(result.data.message);
                 setDogs(dogs);
-                setIsLoaded(true);
                 localStorage.setItem("dogs", JSON.stringify(dogs));
             })
             .catch(error => {
-                setIsLoaded(true);
-                setError(error);
+                console.log(error);
             });
     }, []);
 
@@ -35,34 +31,28 @@ function Dogs() {
         });
     }
 
-    if (error) {
-        return <>{error.message}</>;
-    } else if (!isLoaded) {
-        return <>loading...</>;
-    } else {
-        return (
-            <div>
-                <Header />
-                <div className="container">
-                    <div className="input-group">
-                        <form onSubmit={e => { e.preventDefault(); }}>
-                            <input type="text" className="searchbar" placeholder="Search" name="search" value={q} onChange={(e) => setQ(e.target.value)} />
-                        </form>
-                    </div>
-                    <div className="list-group">
-                        <ul className="card-grid">
-                            {search(dogs).map((dog, i) => (
-                                <Link key={i} type="button" to={{ pathname: '/dogsdetail', state: { dog } }} className="list-group-item list-group-item-action" value={dog}>
-                                    {dog}
-                                </Link>
-                            ))}
-                        </ul>
-                    </div>
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <div className="input-group">
+                    <form onSubmit={e => { e.preventDefault(); }}>
+                        <input type="text" className="searchbar" placeholder="Search" name="search" value={q} onChange={(e) => setQ(e.target.value)} />
+                    </form>
                 </div>
-                <Footer />
+                <div className="list-group">
+                    <ul className="card-grid">
+                        {search(dogs).map((dog, i) => (
+                            <Link key={i} type="button" to={{ pathname: '/dogsdetail', state: { dog } }} className="list-group-item list-group-item-action" value={dog}>
+                                {dog}
+                            </Link>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        );
-    }
+            <Footer />
+        </div>
+    );
 }
 
 export default Dogs;
